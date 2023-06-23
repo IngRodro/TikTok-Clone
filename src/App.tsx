9b9caf70068dfda55dc856c4e5b5c7cd75207828
Rodrigo Ramirez <rr0213032019@unab.edu.sv>
2023-06-23 17:47:46 -0600

    feat(Root): Start project of TikTok clone

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+const App = (): JSX.Element => {
+  return (
+    <div className='App'>
+      <main>
+        Hello, world!
+      </main>
+    </div>
+  )
+}
+
+export default App
